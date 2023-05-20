b5eacaabc56289e616916b333d9043c411bd19de
Mihnea03 <stef1eds@gmail.com>
2023-05-20 17:44:03 +0300

    Main menu functionality

--- src/passpal.c
+++ src/passpal.c
@@ -1,16 +1,27 @@
 #include "utils.h"
 
+#define clear() printf("\033[H\033[J")
+
 void print_main_menu() {
     printf("1. Log In\n");
     printf("2. Sign Up\n");
-    
+    printf("3. Deactivate Account\n");
+    printf("4. Reactivate Account\n");
+    printf("5. Delete Account\n");
+
+    printf("\n");
+
+    printf("Type 'q' to quit the program...\n\n");
+
 }
 
 int main() {
     char input = 0;
 
     while (input != 'q') {
-
+        clear();
+        print_main_menu();
+        scanf("%c", &input);
     }
     return 0;
 }
