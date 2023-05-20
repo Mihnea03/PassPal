5d17c0cf6833dbc14e49a08eeb453fcc197cbd8e
Mihnea03 <stef1eds@gmail.com>
2023-05-20 17:27:17 +0300

    Implemented structs for users and passwords

--- src/utils.h
+++ src/utils.h
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <sys/stat.h>
 
 #define MAX_USERNAME 16
 #define MAX_PASSWORD 20
 
-typedef struct user {
-    char* user_name;
+typedef struct pass {
+    char* url;
+    char* saved_pass;
+    struct pass* next;
+} password, *pass_list;
 
-} User;
+typedef struct usr {
+    char* uid;
+    char* user_name;
+    char* hashed_password;
+    bool is_active;
+    password* passwords;
+} user;
