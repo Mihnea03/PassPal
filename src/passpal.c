c74c6c4034d83ae99328ed16abe4e6227e8fbef9
Mihnea03 <stef1eds@gmail.com>
2023-05-20 17:35:23 +0300

    Main menu

--- src/passpal.c
+++ src/passpal.c
@@ -1,5 +1,16 @@
-#include <stdio.h>
+#include "utils.h"
+
+void print_main_menu() {
+    printf("1. Log In\n");
+    printf("2. Sign Up\n");
+    
+}
 
 int main() {
+    char input = 0;
+
+    while (input != 'q') {
+
+    }
     return 0;
 }
