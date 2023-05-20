5b400596ad3998293d5767969b6b019b5e8fb00f
Mihnea03 <stef1eds@gmail.com>
2023-05-20 17:09:43 +0300

    Library for structs and functions

---
+++ src/utils.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#define MAX_USERNAME 16
+#define MAX_PASSWORD 20
+
+typedef struct user {
+    char* user_name;
+
+} User;
