11ff7d4f445e36d495a77d839210af011a74c8a3
Mihnea03 <stef1eds@gmail.com>
2023-05-22 01:20:49 +0300

    Added function for validating passwords

--- src/crypt.c
+++ src/crypt.c
@@ -26,5 +26,35 @@ unsigned char* encrypt(unsigned char* password, unsigned char* key) {
 }
 
 bool validate_pass(unsigned char* password) {
+    bool is_valid = false;
+    bool lower = false, higher = false, number = false, symbol = false;
+    const char SYMBOLS[] = "~`!@#$%^&*()_-+=|\\}]{[\"':;?/>.<,";
 
+    if (strlen(password) < MIN_PASSWORD) {
+        printf("Password needs to be at least 8 characters long!\n");
+        return false;
+    }
+
+    for (int i = 0; i < strlen(password); i++) {
+        if (password[i] >= 'a' && password[i] <= 'z')
+            lower = true;
+        if (password[i] >= 'A' && password[i] <= 'Z')
+            higher = true;
+        if (password[i] >= '0' && password[i] <= '9')
+            number = true;
+        if (strchr(SYMBOLS, password[i]))
+            symbol = true;
+    }
+
+    if (lower == true && higher == true && number == true && symbol == true)
+        return true;
+
+    if (lower == false)
+        printf("Your password needs to contain at least one lowercase letter!\n");
+    if (higher == false)
+        printf("Your password needs to contain at least one uppercase letter!\n");
+    if (number == false)
+        printf("Your password needs to contain at least one digit!\n");
+    if (symbol == false)
+        printf("Your password needs to contain at least one symbol!\n");
 }
